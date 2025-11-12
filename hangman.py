91abe8ef53237a88c8b3fae6b85f1c64a5f32559
tzadok <tzadok@home.tail3d8167.ts.net>
2025-11-12 11:10:58 +0200

    hangman works almost complete

--- hangman.py
+++ hangman.py
@@ -1,26 +1,41 @@
 #!/bin/python3
 import random
-
 def check_word(word):
-	if guessed_letters==word_to_guess:
+	if word==word_to_guess:
 		print("you won")
-		
+		end=True	
+		return end
 
+def check_index_in_string(word_to_guess,letter):
+	index=word_to_guess.index(letter)
+#	print(word_to_guess)
+#	print(letter)
+	print(index)
+	return index
+
+end=False
 list_of_words=["oren","red","yellow","bus"]
 word_to_guess=random.choice(list_of_words)
 #print(word_to_guess)
 errors=0
-guessed_word=""
-while(errors<5):
+used_letters=[]
+solution=""
+while(errors<5 and not end):
 	letter=input("enter a letter  ")
-	if letter in guessed_letters:
+	if letter in used_letters:
 		print(f"the letter {letter} was chosen before, choose another letter")	
 	else:
 		if letter in word_to_guess:
-			print(f"the letter {letter} exists ")
+			print(f"the letter {letter} exists")
+			index=check_index_in_string(word_to_guess,letter)
+			print(solution[:index])
+			solution=solution[:index]+letter+solution[index:]
+			print(solution)
 		else:
 			print("wrong")
 			errors+=1
-		guessed_letters.append(letter)
-	print(guessed_letters)
-	check_word(guessed_letters)
+		used_letters.append(letter)
+			
+#		check_guess(word_to_guess,used_letters,solution)
+	print(used_letters)
+	end=check_word(solution)
